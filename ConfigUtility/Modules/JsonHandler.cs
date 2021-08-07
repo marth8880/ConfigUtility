b72835f2baddf0498650f0f64a5c88800326cbca
Aaron Gilbert (marth8880) <marth8880@gmail.com>
2021-08-07 16:58:19 -0700

    Config validation and general error-checking #6

--- ConfigUtility/Modules/JsonHandler.cs
+++ ConfigUtility/Modules/JsonHandler.cs
@@ -25,24 +25,44 @@ namespace ConfigUtility
 
 			JObject jObj = JObject.Parse(File.ReadAllText(configPath));
 
-			modConfig.MungedScriptFileName = (string)jObj["mungedScriptFileName"];
-			modConfig.UserConfigLuaTableName = (string)jObj["userConfigLuaTableName"];
-
-			if (modConfig.MungedScriptFileName == "")
+			if (jObj["mungedScriptFileName"] == null || (string)jObj["mungedScriptFileName"] == "")
 			{
-				MessageBox.Show(string.Format("config.json: Invalid or empty mungedScriptFileName was specified. Defaulting to '{0}'.", DEFAULT_MUNGED_SCRIPT_FILE_NAME), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				MessageBox.Show(string.Format("config.json: Invalid or empty 'mungedScriptFileName' was specified or definition is missing. Defaulting to '{0}'.", DEFAULT_MUNGED_SCRIPT_FILE_NAME), "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
 				modConfig.MungedScriptFileName = DEFAULT_MUNGED_SCRIPT_FILE_NAME;
 			}
-
-			if (modConfig.UserConfigLuaTableName == "" || modConfig.UserConfigLuaTableName == "_G")
+			else
 			{
-				MessageBox.Show(string.Format("config.json: Invalid or empty userConfigLuaTableName was specified. Defaulting to '{0}'.", DEFAULT_USER_CONFIG_LUA_TABLE_NAME), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				modConfig.MungedScriptFileName = (string)jObj["mungedScriptFileName"];
+			}
+
+			if (jObj["userConfigLuaTableName"] == null || (string)jObj["userConfigLuaTableName"] == "" || (string)jObj["userConfigLuaTableName"] == "_G")
+			{
+				MessageBox.Show(string.Format("config.json: Invalid or empty 'userConfigLuaTableName' was specified or definition is missing. Defaulting to '{0}'.", DEFAULT_USER_CONFIG_LUA_TABLE_NAME), "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
 				modConfig.UserConfigLuaTableName = DEFAULT_USER_CONFIG_LUA_TABLE_NAME;
+			}
+			else
+			{
+				modConfig.UserConfigLuaTableName = (string)jObj["userConfigLuaTableName"];
+			}
+
+			if (jObj["configTabs"] == null)
+			{
+				MessageBox.Show("config.json: No tabs were found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Environment.Exit(4);
 			}
 
 			// populate tabs
 			foreach (var tab in jObj["configTabs"])
 			{
+				if (tab["name"] == null)
+					DefinitionError("new tab", "name");
+				if (tab["description"] == null)
+					DefinitionError((string)tab["name"], "description");
+				if (tab["footnote"] == null)
+					DefinitionError((string)tab["name"], "footnote");
+				if (tab["flags"] == null)
+					DefinitionError((string)tab["name"], "flags");
+
 				ConfigTab configTab = new ConfigTab();
 				configTab.Name = (string)tab["name"];
 				configTab.Description = (string)tab["description"];
@@ -51,19 +71,56 @@ namespace ConfigUtility
 				// populate flags
 				foreach (var flag in tab["flags"])
 				{
+					if (flag["name"] == null)
+						DefinitionError("new flag", "name");
+					if (flag["path"] == null)
+						DefinitionError((string)flag["name"], "path");
+					if (flag["values"] == null)
+						DefinitionError((string)flag["name"], "values");
+					if (flag["defaultValue"] == null)
+						DefinitionError((string)flag["name"], "defaultValue");
+
 					ConfigFlag configFlag = new ConfigFlag();
 					configFlag.Name = (string)flag["name"];
 					configFlag.Path = (string)flag["path"];
 					configFlag.Values = flag["values"].ToObject<string[]>();
 					configFlag.DefaultValue = (int)flag["defaultValue"];
 
+					if (configFlag.Values.Length == 0)
+					{
+						ValueError(configFlag.Name, "values");
+					}
+
 					configTab.Flags.Add(configFlag);
+				}
+
+				if (configTab.Flags.Count == 0)
+				{
+					ValueError(configTab.Name, "flags");
 				}
 
 				modConfig.Tabs.Add(configTab);
 			}
 
+			if (modConfig.Tabs.Count == 0)
+			{
+				MessageBox.Show("config.json: No tabs were found.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+				Environment.Exit(4);
+			}
+
 			return modConfig;
+		}
+
+		static void DefinitionError(string parentName, string valueType)
+		{
+			MessageBox.Show(string.Format("config.json: No '{0}' definition was found for '{1}'.", valueType, parentName), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			Environment.Exit(4);
+		}
+
+		static void ValueError(string parentName, string flagName)
+		{
+			MessageBox.Show(string.Format("config.json: Invalid '{0}' value specified for '{1}'.", flagName, parentName), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+			Environment.Exit(4);
 		}
 	}
 }
